c1ebc83ecac1d558214ed0c7b6026ae991fa416d
collagePC <ateruimashin2go@gmail.com>
2021-11-25 23:15:28 +0900

    表示桁数の調整と戻り値の追加

--- js/calculateFq.js
+++ js/calculateFq.js
@@ -14,13 +14,15 @@ function getValues() {
   let rl = document.getElementById("rlValue").value;
   //コンデンサ容量を取得
   let c = document.getElementById("cValue").value * Math.pow(10, -9);
+  //可変抵抗の値を取得
+  const r2 = document.getElementById("r2Value").value;
   //リストで返す
-  return [l, r1, rl, c];
+  return [l, r1, rl, r2, c];
 }
 
 function calculateSeriesFq() {
   //共通の値を取得
-  const [l, r1, rl, c] = getValues();
+  const [l, r1, rl, r2, c] = getValues();
   //直列回路の共振周波数を計算する
   const seriesFq = 1 / (2 * pi * Math.sqrt(l * c));
   //innerTextでHTMLに表示する
@@ -30,9 +32,7 @@ function calculateSeriesFq() {
 
 function calculateParallelFq() {
   //共通の値を取得
-  const [l, r1, rl, c] = getValues();
-  //可変抵抗の値を取得
-  const r2 = document.getElementById("r2Value").value;
+  const [l, r1, rl, r2, c] = getValues();
   //Rの計算
   const R = rl + r2;
   //f0の計算
@@ -42,6 +42,7 @@ function calculateParallelFq() {
       Math.sqrt(1 + (2 * c * R * R) / l) / (l * c) - (R * R) / (l * l)
     ) /
     (2 * pi);
+  /*出力 */
   target1 = document.getElementById("parallelF0Result");
   target2 = document.getElementById("parallelFminResult");
   target1.innerText = parallelf0.toPrecision(7) + "Hz";
